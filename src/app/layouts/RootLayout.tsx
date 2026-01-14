96fd9d01d35ca23298047e8b728c0698740e5742
christianleng <christian.leng@epitech.eu>
2026-01-14 13:10:26 +0100

    refactor(): update header, main navigation and search bar design improvements

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,11 +1,11 @@
 import { useIsMobile } from "@/core/hooks/use-mobile";
 import { AppSidebar } from "@/app/layouts/sidebar/appSidebar";
 import Footer from "@/app/layouts/components/Footer";
-import Header from "@/app/layouts/components/Header";
 import { SidebarInset, SidebarProvider } from "@/core/ui/sidebar";
 import { Outlet } from "react-router-dom";
-import Search from "@/features/search/Search";
-import { Separator } from "@/core/ui/separator";
+import TopBar from "@/features/header/components/TopBar";
+import MainNavigation from "@/features/header/components/MainNavigation";
+import SearchBar from "@/features/header/components/SearchBar";
 
 export function RootLayout() {
   const isMobile = useIsMobile();
@@ -14,13 +14,25 @@ export function RootLayout() {
     <SidebarProvider>
       {isMobile && <AppSidebar />}
       <SidebarInset className="flex flex-col min-h-screen">
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <Header />
+        <TopBar />
+
+        <header
+          className="
+          sticky top-0 z-50
+          flex h-16 shrink-0 items-center gap-2
+          bg-header-bg
+          backdrop-blur-[20px]
+          border-b border-header-border
+          transition-[width,height] ease-linear
+          group-has-data-[collapsible=icon]/sidebar-wrapper:h-12
+        "
+        >
+          <div className="container max-w-4/5 mx-auto px-4 py-4">
+            <MainNavigation />
+          </div>
         </header>
 
-        <Search />
-
-        <Separator />
+        <SearchBar />
 
         <main className="flex-1">
           <Outlet />
